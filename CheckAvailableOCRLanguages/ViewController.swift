a686dec9017628bd6741f511e2d83e27be073189
Chris Vasselli <clindsay@gmail.com>
2019-11-12 06:51:09 -0500

    Initial Commit

---
+++ CheckAvailableOCRLanguages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckAvailableOCRLanguages
+//
+//  Created by Chris Vasselli on 11/12/19.
+//  Copyright © 2019 Serpenti Sei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
