16d2d9135c5bbc24cd6c69eac8ed996bc279cc67
Chris Vasselli <clindsay@gmail.com>
2019-11-12 07:00:35 -0500

    Add project that prints out available languages for on-device OCR.

--- CheckAvailableOCRLanguages/ViewController.swift
+++ CheckAvailableOCRLanguages/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let accurateLanguages = try! VNRecognizeTextRequest.supportedRecognitionLanguages(for: .accurate, revision: VNRecognizeTextRequestRevision1)
+
+        let fastLanguages = try! VNRecognizeTextRequest.supportedRecognitionLanguages(for: .fast, revision: VNRecognizeTextRequestRevision1)
+        
+        let text = "Supported languages for accuracy: \(accurateLanguages.joined(separator: ", "))\n\nSupported languages for speed: \(fastLanguages.joined(separator: ", "))"
+        label.text = text
     }
 
 
